487e9f0dd1ddc9062fbfca112c78c1ba06331a0f
woong <woongdam777@gmail.com>
2023-02-27 00:24:07 +0900

    20230227_homejavastart

---
+++ 01_java/a001_base/src/CharacterEx.java
@@ -0,0 +1,32 @@
+public class CharacterEx {
+
+
+    public void CharacterEx1(){
+        char ch1 = 'A';
+        System.out.println(ch1);
+        System.out.println((int)ch1);
+
+
+        char ch2 = 66;
+        System.out.println(ch2);
+
+
+        int ch3 = 67;
+        System.out.println(ch3);
+        System.out.println((char)ch3);
+
+    }
+
+    public void CharacterEx2(){
+        char ch1 = '한';
+        char ch2 = '\uD55C';
+
+        System.out.println(ch1);
+        System.out.println(ch2);
+
+
+    }
+
+
+
+}
---
+++ 01_java/a001_base/src/ExRun.java
@@ -0,0 +1,12 @@
+public class ExRun {
+
+    public static void main(String[] args) {
+
+        CharacterEx ce = new CharacterEx();
+
+//        ce.CharacterEx1();
+        ce.CharacterEx2();
+
+
+    }
+}
