66688348d6931367bfd05654873c24a6af730cfe
home <woongdam777@gmail.com>
2023-11-01 02:12:41 +0900

    canvas_2_color change_filldrawswap

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,3 +1,4 @@
+const modeBtn = document.getElementById("mode-btn");
 const colorOptions=Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
@@ -6,7 +7,9 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
+
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event){
     if(isPainting){
@@ -46,14 +49,35 @@ function onColorClick(event){
     ctx.fillStyle = colorValue;
     color.value = colorValue;
 }
+
+function onModeClick(){
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Draw";
+    }else{
+        isFilling = true;
+        modeBtn.innerText = "Fill";
+    }
+}
+
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0,0,800,800);
+    }
+}
+
 canvas.addEventListener("mousemove",onMove);
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
 canvas.addEventListener("mouseleave",cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+
 lineWidth.addEventListener("change",onLineWidthChange);
 color.addEventListener("change",onColorChange);
 
 colorOptions.forEach(color => color.addEventListener("click",onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
 
 
 
