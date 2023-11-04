b37f7544ea3a8740c97479ee6c822a4c85291dbc
home <woongdam777@gmail.com>
2023-11-05 04:32:37 +0900

    canvas3

--- canvas/canvas.js
+++ canvas/canvas.js
@@ -1,13 +1,23 @@
 const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const fileInput = document.getElementById("file");
+const textInput = document.getElementById("text");
+const saveBtn = document.getElementById("save");
+
 const colorOptions=Array.from(document.getElementsByClassName("color-option"));
 const color = document.getElementById("color");
 const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.width = 800;
-canvas.height = 800;
-ctx.lineWidth = lineWidth.value;
 
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
+
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = lineWidth.value;
+ctx.lineCap = "round";
 let isPainting = false;
 let isFilling = false;
 
@@ -62,11 +72,57 @@ function onModeClick(){
 
 function onCanvasClick(){
     if(isFilling){
-        ctx.fillRect(0,0,800,800);
+        ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
     }
 }
 
-canvas.addEventListener("mousemove",onMove);
+function onDestroyClick(){
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+}
+
+function onEraserClick(){
+    ctx.strokeStyle = "White";
+    isFilling = false;
+    modeBtn.innerText = "Fill";
+}
+
+function onFileChange(event){
+    // console.dir(event.target);
+    const file = event.target.files[0];
+    const url = URL.createObjectURL(file);
+    // console.log(url);
+    const image = new Image();
+    image.src = url;
+    image.onload = function(){
+        ctx.drawImage(image, 0,0, CANVAS_WIDTH,CANVAS_HEIGHT);
+        fileInput.value = null;
+    }
+}
+
+function onDoubleClick(event){
+    // console.log(event.offsetX, event.offsetY);
+    const text = textInput.value;
+    if(text !== ""){
+        ctx.save();
+        ctx.lineWidth = 1;
+        ctx.font = "48px serif";
+        ctx.fillText(text, event.offsetX, event.offsetY);
+        ctx.restore();
+    }
+}
+
+function onSaveClick(){
+    // console.log(canvas.toDataURL());
+    const url = canvas.toDataURL();
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "myDrawing.png"
+    a.click();
+}
+
+canvas.addEventListener("dblclick", onDoubleClick);
+canvas.addEventListener("mousemove",onMove);  // canvas.onmousemove = onMove; 동일
 canvas.addEventListener("mousedown",startPainting);
 canvas.addEventListener("mouseup",cancelPainting);
 canvas.addEventListener("mouseleave",cancelPainting);
@@ -78,8 +134,10 @@ color.addEventListener("change",onColorChange);
 colorOptions.forEach(color => color.addEventListener("click",onColorClick));
 
 modeBtn.addEventListener("click", onModeClick);
-
-
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+fileInput.addEventListener("change", onFileChange);
+saveBtn.addEventListener("click", onSaveClick);
 
 // ctx.fillRect(210, 200, 15, 100);
 // ctx.fillRect(350, 200, 15, 100);
