26e4dc9357f56a8277da8692dbfcbd107d21aab0
home <woongdam777@gmail.com>
2023-12-01 02:48:32 +0900

    coding_programmers_1

--- kakao_coding/Coding/src/kakao/home/coding/ex4.java
+++ kakao_coding/Coding/src/kakao/home/coding/ex4.java
@@ -4,12 +4,38 @@ import java.util.*;
 public class ex4 {
 	public static void main(String[] args) {
 		
-		int num = 13;
-		int result = 0;
+		int[] food = {1,2};
 		
-		for(int i = 0 ; i<num.length; i++) {
-			
-		}
+        String answer = "";
         
+        List<Integer> arr = new ArrayList<>();
+        
+        int fod = food[0];
+        int count = 1;
+                
+        for(int i=1; i<food.length; i++){
+            if(fod == food[i]){
+                count++;
+                if(count%2 == 0)arr.add(fod);
+            }else{
+                fod = food[i];
+                count = 1;               
+            }
+        }
+
+        for(int y=0; y<arr.size();y++){
+            answer += arr.get(y);
+        }
+        
+        String reverse = "";
+        for(int y=arr.size()-1; y>=0;y--){
+            reverse += arr.get(y);
+        }
+        
+        answer = answer + "0" + reverse;
+        
+	    System.out.println(answer);    
+
+	    
 	}
 }
