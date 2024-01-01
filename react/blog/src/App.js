83a2fe3b2f3cb496ce60a4dcf3c9b07345afb60d
home <woongdam777@gmail.com>
2024-01-01 21:45:25 +0900

    react_start

---
+++ react/blog/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+
+  let post =`강남 우동 맛집`;
+  
+
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <h4>블로그임</h4>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>12월 31일 발행</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
