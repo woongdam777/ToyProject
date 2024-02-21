bb76c8fdaebce1c518744aa861d12603e3b313d2
home <woongdam777@gmail.com>
2024-02-21 14:14:52 +0900

    api_success

--- api/goods.js
+++ api/goods.js
@@ -1,17 +1,25 @@
-let xhr = new XMLHttpRequest();
-let url = 'http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire'; /*URL*/
-let queryParams = '?' + encodeURIComponent('serviceKey') + '='+'서비스키'; /*Service Key*/
-queryParams += '&' + encodeURIComponent('Q0') + '=' + encodeURIComponent('서울'); /**/
-queryParams += '&' + encodeURIComponent('Q1') + '=' + encodeURIComponent('종로'); /**/
-queryParams += '&' + encodeURIComponent('QT') + '=' + encodeURIComponent('1'); /**/
-queryParams += '&' + encodeURIComponent('QN') + '=' + encodeURIComponent(''); /**/
-queryParams += '&' + encodeURIComponent('ORD') + '=' + encodeURIComponent('NAME'); /**/
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('15'); /**/
-xhr.open('GET', url + queryParams);
-xhr.onreadystatechange = function () {
+const btn = document.querySelector('.btn>button');
+
+btn.addEventListener('click',()=>{
+
+  let add1 = document.querySelector('[name="address1"]').value;
+  let add2 = document.querySelector('[name="address2"]').value;
+  let week = document.querySelector('[name="week"]').value;
+
+  let xhr = new XMLHttpRequest();
+  let url = 'http://apis.data.go.kr/B552657/ErmctInsttInfoInqireService/getParmacyListInfoInqire'; /*URL*/
+  let queryParams = '?' + encodeURIComponent('serviceKey') + '='+'서비스키'; /*Service Key*/
+  queryParams += '&' + encodeURIComponent('Q0') + '=' + encodeURIComponent(add1); /**/
+  queryParams += '&' + encodeURIComponent('Q1') + '=' + encodeURIComponent(add2); /**/
+  queryParams += '&' + encodeURIComponent('QT') + '=' + encodeURIComponent(week); /**/
+  queryParams += '&' + encodeURIComponent('QN') + '=' + encodeURIComponent(''); /**/
+  queryParams += '&' + encodeURIComponent('ORD') + '=' + encodeURIComponent('NAME'); /**/
+  queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('15'); /**/
+
+  xhr.open('GET', url + queryParams);
+  xhr.onreadystatechange = function () {
     if (this.readyState == 4) {
-      // alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
       let xmlDoc = new DOMParser().parseFromString(this.responseText, "text/xml");
       let items = xmlDoc.getElementsByTagName("item");
 
@@ -26,10 +34,9 @@ xhr.onreadystatechange = function () {
           let contentElements = document.querySelector(".primary>table>tbody");
           contentElements.appendChild(resultTr);
       }
-
-      
-
     }
-};
+  };
 
-xhr.send('');
+  xhr.send('');
+})
+
